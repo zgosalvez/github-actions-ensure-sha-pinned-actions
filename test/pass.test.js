b211c2cb7aeb7ded70f70f659472fddf99b621f1
Zennon Gosalvez <1798166+zgosalvez@users.noreply.github.com>
2022-02-13 11:35:53 +0800

    Adds tests

--- test/pass.test.js
+++ test/pass.test.js
@@ -5,13 +5,16 @@ const path = require('path');
 
 const ip = path.join(__dirname, '../src/index.js');
 const workflowsPath = 'ZG_WORKFLOWS_PATH';
+const allowlist = 'INPUT_ALLOWLIST';
 
 jest.beforeEach(() => {
     process.env[workflowsPath] = 'test/stub/pass';
+    process.env[allowlist] = "aws-actions/\ndocker/login-action";
 });
 
 jest.afterEach(() => {
     delete process.env[workflowsPath];
+    delete process.env[allowlist];
 });
 
 jest.test('actions pass', () => {
